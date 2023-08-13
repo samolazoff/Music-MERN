703671f2de82807a373e1c7311ed16edf3172c3b
unknown <samolazov.roman.alex@gmail.com>
2023-08-13 14:40:52 +0300

    FEAT: header

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,24 +2,43 @@ import React from 'react';
 import {NavLink } from 'react-router-dom';
 
 const links = [
-    ['home', '/'],
-    ['music', '/music'],
-    ['sign in', '/app/login'],
-    ['sign up', '/app/registr'],
-    ['profile', '/app/me'],
+    // title, href, isAuth, !isAuth
+    ['home', '/', true, true],
+    ['music', '/music', true, false],
+    ['sign in', '/app/login', false, true],
+    ['sign up', '/app/registr', false, true],
+    ['profile', '/app/me', true, false],
+    ['exit', 'exit', true, false]
 ];
 
 const Navbar = () => {
+
+    const isAuth= true;
+
     return (
         <nav className="app-navbar h-20">
             <ul className=' flex flex-row justify-end gap-5 items-center uppercase h-full'>
                 {
                     links.map((link, index)=>{
-                        return(
-                            <li key={index} className='nav__item px-3'>
-                                <NavLink to={link[1]}>{link[0]}</NavLink>
-                            </li>
-                        )
+                        if(isAuth){
+                            if(link[2]){
+                                return(
+                                    <li key={index} className='nav__item px-3 hover:bg-link-1-bgc-hover h-full flex justify-center items-center'>
+                                        <NavLink to={link[1]}>{link[0]}</NavLink>
+                                    </li>
+                                )
+                            }
+                            
+                        } else{
+                            if(link[3]){
+                                return(
+                                    <li key={index} className='nav__item px-3 hover:bg-link-1-bgc-hover h-full flex justify-center items-center'>
+                                        <NavLink to={link[1]}>{link[0]}</NavLink>
+                                    </li>
+                                )
+                            }
+                        }
+                       
                     })
                 }
             </ul>
